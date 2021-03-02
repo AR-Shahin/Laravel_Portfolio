852ef2c8d5d59276cede34c90ed9fb4076f5274e
AR-Shahin <mdshahinmije96@gmail.com>
2021-03-02 21:37:21 +0600

    add cache route

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,10 @@ Route::get('/','HomeController@index')->name('home');
 Route::get('/projects', 'ProjectController@index')->name('projects.index');
 Route::get('project/category/{slug}','ProjectController@categoryWiseProject')->name('category-wise-project');
 
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
 
 
